63737c1851986a7f3c0a1bc074bd41a7186f30bc
Arpan Ojha <arpanojha@MacBook-Pro-563.local>
2023-05-17 18:32:35 -0400

    adding files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+var backend_url = 'http://banana-lb-1345717375.us-east-2.elb.amazonaws.com:8000/'
 document.addEventListener('DOMContentLoaded', function() {
   var submitButton = document.getElementById('submit-button');
   submitButton.addEventListener('click', function() {
@@ -6,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Capitalize the input text
     var capitalizedText = inputText.toUpperCase();
-    const url = 'http://localhost:8000/banana/?question='+inputText;
+    const url = backend_url+'banana/?question='+inputText;
 
     fetch(url)
     .then(response => response.json())
@@ -30,22 +32,38 @@ document.addEventListener('DOMContentLoaded', function() {
       var currentTab = tabs[0];
       var currentUrl = currentTab.url;
       const fileUrl = currentUrl;
-      fetch(fileUrl)
-      .then(response => response.blob())
-      .then(blob => {
-        const formData = new FormData();
-        formData.append('file', blob, 'example.pdf');
-        const options = {
-          method: 'POST',
-          body: formData
-      };
-      return fetch('http://localhost:8000/uploadfile/', options);
-  })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-      var outputDiv = document.getElementById('pdfoutcome');
-      outputDiv.innerHTML = '<strong>Current Text:</strong> ' + currentUrl;
+
+      const url = backend_url+'send_me_url/?url='+currentUrl;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    // Store the response in a variable
+    const posts = data;
+    var outputDiv = document.getElementById('pdfoutcome');
+      outputDiv.innerHTML = '<strong>Current Text:</strong> ' + currentUrl+" "+"Uploaded ";
+    // Do something with the response data
+    console.log(posts);
+    return posts
+})
+    .catch(error => console.error(error));
+
+  //     fetch(fileUrl)
+  //     .then(response => response.blob())
+  //     .then(blob => {
+  //       const formData = new FormData();
+  //       formData.append('file', blob, 'example.pdf');
+  //       const options = {
+  //         method: 'POST',
+  //         body: formData
+  //     };
+  //     return fetch('http://127.0.0.1:8000/uploadfile/', options);
+  // })
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  //     .catch(error => console.error(error));
+  //     var outputDiv = document.getElementById('pdfoutcome');
+  //     outputDiv.innerHTML = '<strong>Current Text:</strong> ' + currentUrl;
   });
     
 })
