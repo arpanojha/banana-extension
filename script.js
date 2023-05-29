135c0fd5d378c933b8dfeab7e858442a4369e381
Arpan Ojha <arpanojha@MacBook-Pro-563.local>
2023-05-29 13:39:05 -0400

    added files

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 var backend_url = 'http://banana-lb-1345717375.us-east-2.elb.amazonaws.com:8000/'
+//var backend_url = 'http://127.0.0.1:8000/'
+function generateRandomValue(length) {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+const randomValue = generateRandomValue(16);
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
+  var donateButton = document.getElementById('donate');
+  donateButton.addEventListener('click', function() {
+    window.open('https://checkout.square.site/merchant/ML1BHDZZSY5S3/checkout/V5NTIKJVRL2ATKTKO2HAT6UN', '_blank');
+  });
+
   var submitButton = document.getElementById('submit-button');
   submitButton.addEventListener('click', function() {
     // Get the input text
@@ -8,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Capitalize the input text
     var capitalizedText = inputText.toUpperCase();
-    const url = backend_url + 'banana/?question=' + inputText;
+    const url = backend_url + 'banana/'+randomValue+'/?question=' + inputText;
 
     // Show loading text
     var outputDiv = document.getElementById('output');
@@ -36,32 +57,49 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       var currentTab = tabs[0];
       var currentUrl = currentTab.url;
-      const fileUrl = currentUrl;
-
+  
+      
       // Show loading text
       var outputDiv = document.getElementById('pdfoutcome');
       outputDiv.innerHTML = '<strong>Loading...</strong>';
-
-      const url = backend_url + 'send_me_url/?url=' + currentUrl;
-
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          // Store the response in a variable
-          const posts = data;
-          outputDiv.innerHTML =
-            '<strong>Banana Man Says:</strong> ' + currentUrl + ' Uploaded';
-          // Do something with the response data
-          console.log(posts);
-          return posts;
-          
+    
+      fetch(currentUrl)
+        .then(response => {
+          const contentLength = response.headers.get('content-length');
+          const maxSize = 3 * 1024 * 1024; // Three MB limit
+    
+          if (contentLength && parseInt(contentLength) > maxSize) {
+            outputDiv.innerHTML = '<strong>File size exceeds the limit of 3 MB</strong>';
+            throw new Error('File size exceeds the limit of 3 MB.');
+          }
         })
-        .catch(error => console.error(error))
-        .finally(() => {
-          // Remove loading text
-        
+        .then(() => {
+          const url = backend_url + 'send_me_url/'+randomValue+'/?url=' + currentUrl;
+    
+          fetch(url)
+            .then(response => response.json())
+            .then(data => {
+              // Store the response in a variable
+              const posts = data;
+              outputDiv.innerHTML = '<strong>Current PDF:</strong> ' + currentUrl + ' Uploaded';
+              // Do something with the response data
+              console.log(posts);
+              return posts;
+            })
+            .catch(error => console.error(error))
+            .finally(() => {
+              // Remove loading text
+            });
+        })
+        .catch(error => {
+          outputDiv.innerHTML = '<strong>Error:</strong> ' + error.message;
         });
+
     });
   });
 });
+
+
+
+
 
